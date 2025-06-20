f983626d2b842cfb1b6ed1a9c8593ce5e0f8ba3d
alex <alexander.q.qa.a@mail.ru>
2025-06-20 10:40:53 +0600

    Add preview script for Web Service deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,12 +3,20 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  preview: {
+    host: true,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+  },
+  server: {
+    host: true,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+  }
 })
